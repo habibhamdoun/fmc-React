6c9bb5458d8ede3b266104a1262273c1a800f5be
habibhamdoun <habibhamdoun@gmail.com>
2022-05-04 00:12:18 +0300

    defined services section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import AOS from "aos";
 import Navbar from "./components/Navbar";
+import Vision from "./components/Vision";
+import WhyUs from "./components/WhyUs";
+import Services from "./components/Services";
 
 function App() {
   AOS.init();
   return (
     <div className="App">
       <Navbar />
+      <Services />
     </div>
   );
 }
