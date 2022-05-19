6bc07a161f6d81d8ac7414059d8c37be27e8f8e0
habibhamdoun <habibhamdoun@gmail.com>
2022-05-20 02:30:58 +0300

    hero change pt 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,11 +17,10 @@ function Hero() {
       ></motion.div>
       <div className="hero__content">
         <h1 className="hero__header">
-          First Modern <br /> Company
+          First Modern Company
         </h1>
         <a href="#aboutUs" className="hero__toggle">
           <div className="hero__link">
-            <p>Learn More</p>
             <svg xmlns="http://www.w3.org/2000/svg" className='hero__arrow'viewBox="0 0 448 512"><path d="M224 416c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 338.8l169.4-169.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-192 192C240.4 412.9 232.2 416 224 416z"/></svg>
           </div>
         </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,8 @@ const Navbar = () => {
       animate={{translateY:0}}
     >
     <div className="nav__brand__container">
-      <img src={Image} className="nav__brand" alt="" />
+      {/* <img src={Image} className="nav__brand" alt="" /> */}
+      <h3 className='nav__name'>FMC</h3>
       <div onClick={()=>setIsOpen(old => old ? false : true)} className="nav__collapsible__container">
         <svg xmlns="http://www.w3.org/2000/svg" className="collapsible__toggle" viewBox="0 0 448 512">
           <path d="M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z"/></svg>
