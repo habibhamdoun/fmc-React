d853de2517296c01cc48d8aa1bdbddab63e20954
habibhamdoun <habibhamdoun@gmail.com>
2022-05-25 01:49:46 +0300

    minor fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,15 @@ import { useTranslation } from 'react-i18next';
 const Navbar = () => {
   const {t,i18n}=useTranslation('translation');
   const [isOpen, setIsOpen] = useState(false);
-  const [buttontext ,setButtonText]=useState('EN');
+  const [buttontext ,setButtonText]=useState('ENG');
   function handleClick() {
     if(i18n.language=='en'){
       i18n.changeLanguage('ar');
-      setButtonText('ع');
+      setButtonText('العربية');
     } 
       else{
         i18n.changeLanguage('en');
-        setButtonText('EN');
+        setButtonText('ENG');
       }
     }
   
@@ -26,7 +26,7 @@ const Navbar = () => {
       animate={{translateY:0}}
     >
     <div className="nav__brand__container">
-      <h3 className='nav__name'>FMC</h3>
+    <NavLink onClick={()=> setIsOpen(false)}  to="/"><h3 className='nav__name'>FMC</h3></NavLink>
       <div onClick={()=>setIsOpen(old => old ? false : true)} className="nav__collapsible__container">
         <svg xmlns="http://www.w3.org/2000/svg" className="collapsible__toggle" viewBox="0 0 448 512">
           <path d="M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z"/></svg>
