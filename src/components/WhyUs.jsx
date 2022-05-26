2018feca1b4ba8ed9d96a9d6b93d8a32432964eb
habibhamdoun <habibhamdoun@gmail.com>
2022-05-26 20:29:18 +0300

    why us language +lang css fix

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -5,40 +5,41 @@ import { useTranslation } from 'react-i18next';
 
 
 const WhyUs = () => {
-  const { t ,i18n }=useTranslation('translation');
+  const { t, i18n } = useTranslation('translation');
   return (
-    <motion.section 
-      initial={{translateX:1000}}
-      animate={{translateX:0}}
-      transition={{duration:0.4}}
-      className="whyUs"
+    <motion.section
+      initial={{ translateX: 1000 }}
+      animate={{ translateX: 0 }}
+      transition={{ duration: 0.4 }}
+      className={i18n.language=='ar'?"whyUs whyUs--arabic":"whyUs"}
     >
-    <div className="whyUs__header">
+      <div className={i18n.language=='ar'?"whyUs__header whyUs__header--arabic":"whyUs__header"}>
         <div className="title">
-            <h3 className="title__header whyUs__title">{t('whyUsTitle')}
-            </h3>
-            <div className="line line--blue"></div>
+          {i18n.language=='ar'&&<div className="line line--blue"></div>}
+          <h3 className={i18n.language == 'ar' ? "title__header whyUs__title arabic" : "title__header whyUs__title arabic"}>{t('whyUsTitle')}
+          </h3>
+          {i18n.language=='en'&&<div className="line line--blue"></div>}
         </div>
-        <p className="whyUs__text"> {t('whyUsText')}
-          </p>
-    </div>
-    <Link to="/contact-us">
-      <motion.button 
-        className="btn btn--secondary whyUs__btn" 
-        initial={{translateX:1000}}
-        animate={{translateX:0}}
-        transition={{duration:0.45}}
-      >
-        {t('whyUsBtn')}
-      </motion.button>
-    </Link>
-    <motion.div 
-      initial={{translateX:1000}}
-      animate={{translateX:0}}
-      transition={{duration:0.5}} 
-      className="whyUs__img" 
-    />      
-</motion.section>
+        <p className={i18n.language=='ar'?"whyUs__text arabic":"whyUs__text"}> {t('whyUsText')}
+        </p>
+      </div>
+      <Link to="/contact-us">
+        <motion.button
+          className="btn btn--secondary whyUs__btn"
+          initial={{ translateX: 1000 }}
+          animate={{ translateX: 0 }}
+          transition={{ duration: 0.45 }}
+        >
+          {t('whyUsBtn')}
+        </motion.button>
+      </Link>
+      <motion.div
+        initial={{ translateX: 1000 }}
+        animate={{ translateX: 0 }}
+        transition={{ duration: 0.5 }}
+        className={i18n.language=='ar'?"whyUs__img whyUs__img--arabic":"whyUs__img"}
+      />
+    </motion.section>
   )
 }
 
