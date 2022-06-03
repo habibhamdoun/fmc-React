328703d9aed3baa62c62ad9806554730d42a2aa8
habibhamdoun <habibhamdoun@gmail.com>
2022-06-03 23:07:32 +0300

    nav alignement with language

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import i18n from '../i18n';
 import { useTranslation } from 'react-i18next';
 
-const Navbar = () => {
+const Navbar = ({isMobile}) => {
   const {t,i18n}=useTranslation('translation');
   const [isOpen, setIsOpen] = useState(false);
   const [buttontext ,setButtonText]=useState('ENG');
@@ -25,14 +25,14 @@ const Navbar = () => {
       initial={{translateY:-200}}
       animate={{translateY:0}}
     >
-    <div className="nav__brand__container">
-    <NavLink onClick={()=> setIsOpen(false)}  to="/"><h3 className='nav__name'>FMC</h3></NavLink>
+    {i18n.language=='en' || isMobile ?<div className="nav__brand__container">
+     <NavLink onClick={()=> setIsOpen(false)}  to="/"><h3 className='nav__name'>FMC</h3></NavLink>
       <div onClick={()=>setIsOpen(old => old ? false : true)} className="nav__collapsible__container">
         <svg xmlns="http://www.w3.org/2000/svg" className="collapsible__toggle" viewBox="0 0 448 512">
           <path d="M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z"/></svg>
       </div>
-    </div>
-    <ul className="list list--inline nav__list collapsible__content">
+    </div> : ''}
+    <ul className={i18n.language=='ar'  ? "list list--inline nav__list nav__list--arabic collapsible__content" : "list list--inline nav__list collapsible__content"}>
       <li className="nav__item"><NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} onClick={()=> setIsOpen(false)}  to="/">{t('home')}</NavLink></li>
       <li className="nav__item"><NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} onClick={()=> setIsOpen(false)}   to="/vision"  >{t("vision")}</NavLink></li>
       <li className="nav__item"><NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} onClick={()=> setIsOpen(false)}   to="/mission"  >{t("mission")}</NavLink></li>
@@ -43,6 +43,7 @@ const Navbar = () => {
       <li className="nav__item"><NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} onClick={()=> setIsOpen(false)}  to="/quote" >{t('quote')}</NavLink></li>
       <button className='language__input nav__item' onClick={handleClick}>{buttontext}</button>
     </ul>
+    {i18n.language=='ar' && !isMobile ? <NavLink onClick={()=> setIsOpen(false)}  to="/"><h3 className='nav__name'>FMC</h3></NavLink> : ""}
 
 </motion.nav>
   )
